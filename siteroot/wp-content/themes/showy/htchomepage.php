51fdb49dec972c27d2f667ddd43c49fa879acefa
Daniel Haarburger <work.haarburger@gmail.com>
2013-06-27 10:16:43 -0600

    adds paragraph beneath SIMPLE section, also adds link

--- siteroot/wp-content/themes/showy/htchomepage.php
+++ siteroot/wp-content/themes/showy/htchomepage.php
@@ -13,8 +13,8 @@ Template Name: HTC Homepage
 		</div>
 
 		<div id="home_secondpg">
-				<div id="how_tag">
-					<h3>HERE'S HOW</h3>
+				<div id="how_tag" class="tag">
+					<a href="http://www.google.com"><h3>HERE'S HOW</h3></a>
 				</div>
 				<div id="secondpg_content">
 					<div class="content">
@@ -50,15 +50,32 @@ Template Name: HTC Homepage
 									</a>
 								</li>
 							</ul>
-
-
+						</div> <!-- End Thirds -->
+								<br>
+						<div class="caption_paragraph">
+							<p>
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis 
+								vestibulum. <span>Morbi leo risus, porta ac consectetur ac</span>, vestibulum at eros. <span>Curabitur blandit </span>tempus porttitor. Cum sociis natoque
+								penatibus parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur
+								et. Nullam quis risus <span>et magnis dis</span> eget urna mollis ornare vel eu leo.
+							</p>
+							<a href="http://www.google.com">Here's how to start</a>
 						</div>
-					</div>
-				</div>
-		</div>
+					</div> <!-- End Content -->
+
+				</div><!-- End Secondpg_content -->
+		</div> <!-- End home_secondpg -->
 
 		<div id="home_thridpg">
-			
+			<div id="how_tag" class="tag">
+					<a href="http://www.google.com"><h3>HERE'S HOW</h3></a>
+				</div>
+				<div id="secondpg_content">
+					<div class="content">
+						<h2>Crowdfunding Can Be <span>Simple</span></h2>
+					</div> <!-- End Content -->
+
+				</div><!-- End Secondpg_content -->
 
 		</div>
 </div>
