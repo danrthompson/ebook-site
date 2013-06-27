b0132c24adab0d1f09e0611c003c717a4d00908b
Daniel Haarburger <work.haarburger@gmail.com>
2013-06-27 12:24:06 -0600

    finishes all of the layout of the homepage. Refinement still needed, but layout is good

--- siteroot/wp-content/themes/showy/htchomepage.php
+++ siteroot/wp-content/themes/showy/htchomepage.php
@@ -78,7 +78,7 @@ Template Name: HTC Homepage
 					<hr>
 					<div class="why_sect_block">
 						<div class="thirty left_offset f_left">
-							<h4>WORTH YOUR <span>TIME</span></h4>
+							<h4>WORTH YOUR <span id="orange">TIME</span></h4>
 						</div>
 						<div class="fifty f_right">
 							<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus.</p>
@@ -90,19 +90,21 @@ Template Name: HTC Homepage
 							<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus.</p>
 						</div>
 						<div class="thirty right_offset f_right">
-							<h4><span>YOU CAN</span> DO IT!</h4>
+							<h4><span id="yellow">YOU CAN</span> DO IT!</h4>
 						</div>
 					</div><!-- End Block -->
 					<hr>
 					<div class="why_sect_block">
 						<div class="thirty left_offset f_left">
-							<h4>WE WILL <span>HELP YOU</span></h4>
+							<h4>WE WILL <span id="blue">HELP YOU</span></h4>
 						</div>
 						<div class="fifty f_right">
 							<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus.</p>
 						</div>
 					</div><!-- End Block -->
 					</div> <!-- End Content -->
+					
+					<a href="http://www.google.com" id="lets_go_button">Let's Go!</a>
 				</div><!-- End Secondpg_content -->
 
 		</div>
