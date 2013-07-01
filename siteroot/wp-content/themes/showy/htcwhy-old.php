5c28e16841120094334b12d44882e4db21b3c98d
Daniel Haarburger <work.haarburger@gmail.com>
2013-07-01 17:27:07 -0600

    creates why page, populates with placeholder content

---
+++ siteroot/wp-content/themes/showy/htcwhy-old.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: Why
+*/
+?>  
+<?php get_header(); ?>
+
+
+<div id="thirdpg_content" class="pgcontent">
+		<div class="content">
+			<h2>Crowdfunding Is <span>Important</span></h2>
+			<br>
+			<br>
+			<hr>
+			<div class="why_sect_block">
+				<div class="thirty left_offset f_left">
+					<h4>WORTH YOUR <span id="orange">TIME</span></h4>
+				</div>
+				<div class="fifty f_right">
+					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus.</p>
+				</div>
+			</div><!-- End Block -->
+			<hr>
+			
+			<div class="why_sect_block">
+				<div class="fifty f_left">
+					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus.</p>
+				</div>
+				<div class="thirty right_offset f_right">
+					<h4><span id="yellow">YOU CAN</span> DO IT!</h4>
+				</div>
+			</div><!-- End Block -->
+			<hr>
+			
+			<div class="why_sect_block">
+				<div class="thirty left_offset f_left">
+					<h4>WE WILL <span id="blue">HELP YOU</span></h4>
+				</div>
+				<div class="fifty f_right">
+					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus.</p>
+				</div>
+			</div><!-- End Block -->
+		</div> <!-- End Content -->
+		
+		<a href="/field-guide" id="lets_go_button">Let's Go!</a>
+	</div><!-- End Secondpg_content -->
+</div>
+
+
+<?php get_footer(); ?>
