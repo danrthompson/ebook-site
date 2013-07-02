be04a934000efb0628e640f62fb720c3de88f8bf
Daniel Haarburger <work.haarburger@gmail.com>
2013-07-01 23:32:48 -0600

    committing before pull

---
+++ siteroot/wp-content/themes/showy/htcebookpage.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Template Name: Ebook Page
+*/
+?>  
+<?php get_header(); ?>
+
+<div id="why_page">
+	<div id="" class="content">
+		
+
+<!--  ///////////// WHY PAGE //////////////// -->
+
+
+<!-- 	<h1>Crowdfunding Is <span>Important</span></h1> -->
+	<div id="why_pg" class="pgcontent">
+			<div class="content block ebook_pg">
+			
+				<div class="why_pg_central_content">
+					<div class="half left-float">
+						<img class="wide_img" id="ebook-img" src="<?php echo get_bloginfo('template_url') ?>/images/book_placeholder.png"/>
+					</div>
+					<div class="half right-float top-margin">
+						<h5 id="left-float"> This Is How To Start</h4>
+						<h5><span class="teal thick-title left-float">LEARNING</span></h4>
+						<div class="accent"></div>
+						<p>
+						Crowdfunding - the collective effort across a group to make a particular idea a reality - is taking the world by storm. In 2011, entrepreneurs raised over $1.5 Billion to make their ideas a reality. This year, that number is expected to grow to $5.1 Billion.
+						</p>
+						<div class="left-float ebook_buttons">
+							<a href="http://google.com" class="float-left button-outline teal">PREVIEW</a>
+							<a href="http://google.com" class="float-left button-solid teal">GET IT!</a>
+						</div>
+					</div>
+				</div><!-- End Block -->
+			</div> <!-- End Content -->
+
+				<div id="why_pg" class="pgcontent">
+			<div class="content block ebook_pg">
+			
+				<div class="why_pg_central_content">
+					<div class="half right-float">
+						<img class="wide_img" id="kit-img" src="<?php echo get_bloginfo('template_url') ?>/images/book_placeholder.png"/>
+					</div>
+					<div class="half right-float top-margin">
+						<h5 id="right-float"> This Is How To Start</h4>
+						<h5><span class="yellow thick-title right-float">LEARNING</span></h4>
+						<div class="accent yellow"></div>
+						<p>
+						Crowdfunding - the collective effort across a group to make a particular idea a reality - is taking the world by storm. In 2011, entrepreneurs raised over $1.5 Billion to make their ideas a reality. This year, that number is expected to grow to $5.1 Billion.
+						</p>
+						<div class="right-float ebook_buttons">
+							<a href="http://google.com" class="float-left button-outline teal">PREVIEW</a>
+							<a href="http://google.com" class="float-left button-solid teal">GET IT!</a>
+						</div>
+					</div>
+				</div><!-- End Block -->
+			</div> <!-- End Content -->
+
+			
+			
+	</div>
+	<div class="bottom-button-wrapper">
+		<a href="/field-guide" id="lets_go_button">Let's Go!</a>
+	</div>
+	<!--  ///////////// END WHY PAGE //////////////// -->
+		
+	<!-- small sidebar -->
+	<!-- <div class="sidebar-wrapper content">
+			<div id="sidebar" class="clearfix">
+				<div class="sidebar-inner">
+					<h1><span>Crowdfunding</span> Resources</h1>
+					<hr>
+					<a href="http://www.google.com">Suggest A Resource</a>
+					<hr>
+					
+					<ul class="sidebar-list">
+						<li class="sidebar-list-entry"><a href="#resource_sect1">Featured Resources</a></li>
+						<li class="sidebar-list-entry"><a href="#resource_sect2">Booklist</a></li>
+						<li class="sidebar-list-entry"><a href="#resource_sect3">About Crowdfunding</a></li>
+						<li class="sidebar-list-entry"><a href="#resource_sect4">Idea Development</a></li>
+						<li class="sidebar-list-entry"><a href="#resource_sect5">Campaign Prep</a></li>
+						<li class="sidebar-list-entry"><a href="#resource_sect6">Manage The Campaign</a></li>
+						<li class="sidebar-list-entry"><a href="#resource_sect7">After The Campaign</a></li>
+					</ul>
+
+					<a href="www.google.com" id="campaign_services_link">Helpful Services</a>
+
+
+				</div>
+			</div>
+		</div>
+ -->
+
+
+</div>
+
+<?php get_footer(); ?>
