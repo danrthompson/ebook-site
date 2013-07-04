d577bb910fbf959b31f4ba9a5039db56e43b4cd8
Daniel Haarburger <work.haarburger@gmail.com>
2013-07-04 16:10:19 -0600

    adds copy to resources page

--- siteroot/wp-content/themes/showy/htcresources.php
+++ siteroot/wp-content/themes/showy/htcresources.php
@@ -9,10 +9,14 @@ Template Name: HTC Resources
 
 	<!--  Resource Content-->
 			<div class="content-posts clearfix">
-	
+				
 				<div class="entry-content">
 					<img src="<?php echo get_bloginfo('template_url') ?>/images/resources_title_img.png"/>
-					
+					<br>
+					<br>
+					<div class="intro"> <p>There are thousands of resources to help you crowdfund your idea. To save you from having to read each and every one of them, we've compile a small library of the ones that are most helpful. Indulge and enjoy.</p>
+					</div>
+
 					<header>
 						<div class="archive-titlewrapper">
 							<h2 class="entry-title" id="resource_sect1">Featured Resources</h2>
@@ -21,7 +25,7 @@ Template Name: HTC Resources
 					
 					<div class="accent"></div>
 					<div class="posts-content">
-						<div class="intro"> Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.</div>
+						<div class="intro">There's a lot to know about crowdfunding. Here are a few resources carefully selected to get you started. </div>
 
 						<h5>Example Title</h5>
 						
@@ -43,40 +47,147 @@ Template Name: HTC Resources
 						</div>
 					</div>
 
-
+				<!-- ///////////////// BOOKLIST /////////////// -->
 					<header>
 						<div class="archive-titlewrapper">
 							<h2 class="entry-title" id="resource_sect2">Booklist</h2>
 						</div>
 					</header>
-					
 					<div class="accent"></div>
 					<div class="posts-content">
-						<div class="intro"> Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo.</div>
+						<div class="intro"> If you're jonesing to get a deeper understanding of crowdfunding as an industry these books will help. Each offers valuable insight into a different facet of crowdfunding and the practices that lead to success. </div>
 
 						<h5>Example Title</h5>
 						
 						<a href=""><h6>Resource Title</h6></a>
 						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 
-						<a href=""><h6>The Title of the Second Resource</h6></a>
-						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-
-						<a href=""><h6>Third Resource Name</h6></a>
-						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 
 						<h5>The Second Sub-Heading Title</h5>
 
 						<a href=""><h6>Resource Title</h6></a>
 						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-						<div class="centered">
-							<a href="http://themes.playnethemes.com/showy/the-new-imac/" class="more-link" title="read more"><i class="icon-angle-right"></i></a>
-						</div>
 					</div>
+				<!-- //////////////////////////////////////// -->
+
+
+				<!-- ///////////////// ABOUT CROWDFUNDING /////////////// -->
+					<header>
+						<div class="archive-titlewrapper">
+							<h2 class="entry-title" id="resource_sect3">ABOUT CROWDFUNDING</h2>
+						</div>
+					</header>
+					<div class="accent"></div>
+					<div class="posts-content">
+						<div class="intro">Crowdfunding is a massive and growing industry. What are the different types? Who is it for? How is it changing? These resources are meant to catch you up to speed.</div>
+
+						<h5>Example Title</h5>
+						
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+
+						<h5>The Second Sub-Heading Title</h5>
+
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					</div>
+				<!-- //////////////////////////////////////// -->
+
+
+				<!-- ///////////////// IDEA DEVELOPMENT /////////////// -->
+					<header>
+						<div class="archive-titlewrapper">
+							<h2 class="entry-title" id="resource_sect4">Idea Development</h2>
+						</div>
+					</header>
+					<div class="accent"></div>
+					<div class="posts-content">
+						<div class="intro">Having an idea is one thing, but knowing who it's for, why it's important, and how to see it made is a whole different animal. These resources are useful for refining your idea before you bring it to market.</div>
+
+						<h5>Example Title</h5>
+						
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+
+						<h5>The Second Sub-Heading Title</h5>
+
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					</div>
+				<!-- //////////////////////////////////////// -->
+
+				<!-- ///////////////// CAMPAIGN PREP /////////////// -->
+					<header>
+						<div class="archive-titlewrapper">
+							<h2 class="entry-title" id="resource_sect5">Campaign Prep</h2>
+						</div>
+					</header>
+					<div class="accent"></div>
+					<div class="posts-content">
+						<div class="intro">A little preparation goes a long way, especially with crowdfunding. Below is advice and information submitted crowdfunding veterans to help guide you before you start your journey.</div>
+
+						<h5>Example Title</h5>
+						
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+
+						<h5>The Second Sub-Heading Title</h5>
+
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					</div>
+				<!-- //////////////////////////////////////// -->
+
+				<!-- ///////////////// Managing The Campaign /////////////// -->
+					<header>
+						<div class="archive-titlewrapper">
+							<h2 class="entry-title" id="resource_sect6">Managing The Campaign</h2>
+						</div>
+					</header>
+					<div class="accent"></div>
+					<div class="posts-content">
+						<div class="intro">Now the fun part! You've refined your idea and prepared your campaign. Now it's time for launch! Here are resources to help you over the course of your campaign.</div>
+
+						<h5>Example Title</h5>
+						
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+
+						<h5>The Second Sub-Heading Title</h5>
+
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					</div>
+				<!-- //////////////////////////////////////// -->
+
+				<!-- ///////////////// IDEA DEVELOPMENT /////////////// -->
+					<header>
+						<div class="archive-titlewrapper">
+							<h2 class="entry-title" id="resource_sect7">After The Campaign</h2>
+						</div>
+					</header>
+					<div class="accent"></div>
+					<div class="posts-content">
+						<div class="intro">So you're a smash hit! Now what? Here are some resources to help you deliver on your project, keep your backers happy, and lay the foundation for future success.</div>
+
+						<h5>Example Title</h5>
+						
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+
+						<h5>The Second Sub-Heading Title</h5>
+
+						<a href=""><h6>Resource Title</h6></a>
+						<p id="post-paragraph"> Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					</div>
+				<!-- //////////////////////////////////////// -->
 
 				</div>
-
-
 			</div>	
 
 	<!-- Sidebar -->
